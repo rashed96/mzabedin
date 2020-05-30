da2740e7d8ca1f0e75cdbbbb346c495d40430688
md.rashad khan <56927662+rashed96@users.noreply.github.com>
2020-05-30 10:35:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,317 @@
+(function($) {
+	
+	"use strict";
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var scrollLink = $('.scroll-to-top');
+			if (windowpos >= 250) {
+				siteHeader.addClass('fixed-header');
+				scrollLink.fadeIn(300);
+			} else {
+				siteHeader.removeClass('fixed-header');
+				scrollLink.fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+	
+	
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown ul').length){
+		$('.main-header li.dropdown').append('<div class="dropdown-btn"><span class="fa fa-angle-down"></span></div>');
+		
+		//Dropdown Button
+		$('.main-header li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('ul').slideToggle(500);
+		});
+		
+		//Disable dropdown parent link
+		$('.main-header .navigation li.dropdown > a,.hidden-bar .side-menu li.dropdown > a').on('click', function(e) {
+			e.preventDefault();
+		});
+	}
+	
+	
+	//Hidden Bar Menu Config
+	function hiddenBarMenuConfig() {
+		var menuWrap = $('.hidden-bar .side-menu');
+		// hidding submenu 
+		menuWrap.find('.dropdown').children('ul').hide();
+		// toggling child ul
+		menuWrap.find('li.dropdown > a').each(function () {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$(this).parent('li.dropdown').children('ul').slideToggle();
+	
+				// adding class to item container
+				$(this).parent().toggleClass('open');
+	
+				return false;
+	
+			});
+		});
+	}
+	
+	hiddenBarMenuConfig();
+	
+	
+	//Hidden Sidebar
+	if ($('.hidden-bar').length) {
+		var hiddenBar = $('.hidden-bar');
+		var hiddenBarOpener = $('.hidden-bar-opener');
+		var hiddenBarCloser = $('.hidden-bar-closer');
+		$('.hidden-bar-wrapper').mCustomScrollbar();
+		
+		//Show Sidebar
+		hiddenBarOpener.on('click', function () {
+			hiddenBar.addClass('visible-sidebar');
+		});
+		
+		//Hide Sidebar
+		hiddenBarCloser.on('click', function () {
+			hiddenBar.removeClass('visible-sidebar');
+		});
+	}
+	
+	
+	//Panels Toggle (Shopping Cart Page)
+	if ($('.toggle-panel').length) {
+		var targetPanel = $('.toggle-content');
+		
+		//Show Panel
+		$('.toggle-panel').on('click', function () {
+			$(this).toggleClass('active-panel');
+			$(this).next(targetPanel).fadeToggle(300);
+		});
+	}
+	
+	
+	//Single Item Carousel
+	if ($('.single-item-carousel').length) {
+		$('.single-item-carousel').owlCarousel({
+			loop:true,
+			margin:4,
+			nav:true,
+			smartSpeed: 700,
+			autoplay: 8000,
+			/*animateOut: 'fadeOut',
+      		animateIn: 'fadeIn',*/
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				800:{
+					items:1
+				},
+				1024:{
+					items:1
+				},
+				1200:{
+					items:1
+				},
+				1300:{
+					items:1
+				}
+			}
+		});    		
+	}
+	
+	
+	//Two Item Carousel
+	if ($('.two-item-carousel').length) {
+		$('.two-item-carousel').owlCarousel({
+			loop:true,
+			margin:30,
+			nav:true,
+			smartSpeed: 700,
+			autoplay: 8000,
+			/*animateOut: 'fadeOut',
+      		animateIn: 'fadeIn',*/
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				800:{
+					items:1
+				},
+				1024:{
+					items:2
+				},
+				1200:{
+					items:2
+				},
+				1300:{
+					items:2
+				}
+			}
+		});    		
+	}
+	
+	
+	//Three Item Carousel
+	if ($('.three-item-carousel').length) {
+		$('.three-item-carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			smartSpeed: 700,
+			autoplay: 8000,
+			/*animateOut: 'fadeOut',
+      		animateIn: 'fadeIn',*/
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				650:{
+					items:2
+				},
+				800:{
+					items:2
+				},
+				1024:{
+					items:2
+				},
+				1200:{
+					items:3
+				},
+				1300:{
+					items:3
+				}
+			}
+		});    		
+	}
+	
+	
+	//Five Item Carousel
+	if ($('.five-item-carousel').length) {
+		$('.five-item-carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
+			smartSpeed: 700,
+			autoplay: 8000,
+			/*animateOut: 'fadeOut',
+      		animateIn: 'fadeIn',*/
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				668:{
+					items:2
+				},
+				800:{
+					items:2
+				},
+				1024:{
+					items:3
+				},
+				1200:{
+					items:4
+				},
+				1400:{
+					items:5
+				}
+			}
+		});    		
+	}
+	
+	
+	//LightBox / Fancybox
+	if($('.lightbox-image').length) {
+		$('.lightbox-image').fancybox({
+			openEffect  : 'fade',
+			closeEffect : 'fade',
+			helpers : {
+				media : {}
+			}
+		});
+	}
+	
+	
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1000);
+	
+		});
+	}
+	
+	//Contact Form Validation
+	if($('#contact-form').length){
+		$('#contact-form').validate({
+			rules: {
+				name: {
+					required: true
+				},
+				email: {
+					required: true
+				},
+				phone: {
+					required: true
+				},
+				message: {
+					required: true
+				}
+			}
+		});
+	}
+	
+	
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       false,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+	
+/* ==========================================================================
+   When document is loaded, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+	});	
+
+})(window.jQuery);
